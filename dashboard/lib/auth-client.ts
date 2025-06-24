1fa54183519773c53dd83bfcb3a7121df4f7e3b6
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2025-06-24 11:11:56 +0200

    restore

--- dashboard/lib/auth-client.ts
+++ dashboard/lib/auth-client.ts
@@ -4,9 +4,7 @@ import {
   inferAdditionalFields,
 } from "better-auth/client/plugins";
 export const authClient = createAuthClient({
-  baseURL:
-    process.env.NEXT_PUBLIC_BETTER_AUTH_URL ||
-    (typeof window !== "undefined" ? window.location.origin : ""),
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL!,
 
   plugins: [emailOTPClient(), inferAdditionalFields()],
 });
