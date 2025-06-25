fcfec58f0073f26bd29291962cb1b519ed68ec40
Jonas Lorenz <jonas@lorenz.place>
2025-06-25 17:56:51 +0200

    jordan >> lebron

--- backend/query.py
+++ backend/query.py
@@ -28,7 +28,7 @@ class QueryDB():
         self.scope = os.getenv("SCOPE_NAME")
         self.collect = os.getenv("COLLECTION_NAME")
 
-    def get_requests_by_ids(self, api_key: str, requests_per_session: int) -> dict[str, list[dict]]:
+    def get_requests_by_ids(self, session_id: str, requests_per_session: int) -> list[dict]:
         """
         Query PostgreSQL for specific request rows by their IDs.
         
@@ -48,41 +48,26 @@ class QueryDB():
 
         try:
 
-            query_apps = f"""
-            SELECT DISTINCT session_id
-            FROM `{self.bucket}`.`{self.scope}`.`{self.collect}`
-            WHERE api_key = $api_key;
-            """
-            result = cluster.query(query_apps, QueryOptions(named_parameters={
-                "api_key": api_key,
+            per_sesh_query = f"""
+                SELECT *
+                FROM `{self.bucket}`.`{self.scope}`.`{self.collect}`
+                WHERE session_id = $session_id
+                ORDER BY timestamp DESC
+                LIMIT $number;
+                """
+                
+            result = cluster.query(per_sesh_query, QueryOptions(named_parameters={
+                "session_id": session_id,
+                "number": requests_per_session
             }))
 
-            session_id_list = [row["session_id"] for row in result]
+            json_list = [row for row in result]
 
-            grouped: dict[str, list[dict]] = defaultdict(list)
-            
-            for session_id in session_id_list:
-                per_sesh_query = f"""
-                                SELECT *
-                                FROM `{self.bucket}`.`{self.scope}`.`{self.collect}`
-                                WHERE session_id = $session_id AND api_key = $api_key
-                                ORDER BY timestamp DESC
-                                LIMIT $number;
-                                """
-                
-                result = cluster.query(per_sesh_query, QueryOptions(named_parameters={
-                    "session_id": session_id,
-                    "api_key": api_key,
-                    "number": requests_per_session
-                }))
-
-                grouped[str(session_id)] = [row for row in result]
-
-            return grouped
+            return json_list
             
         except Exception as e:
             print(f"Database query error: {e}")
-            return {}
+            return []
 
 
     def get_all_user_requests(user_id: str, limit: int = 100) -> list[dict[str, Any]]: # type: ignore
